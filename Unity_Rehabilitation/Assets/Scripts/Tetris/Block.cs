e7f79e9074b8c05e03628a07702d5e340d835f29
iyokurot <01kayochin17neko@gmail.com>
2021-11-07 05:14:36 +0900

    ブロックの動き作成

---
+++ Unity_Rehabilitation/Assets/Scripts/Tetris/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+        // Observable.Interval (TimeSpan.FromMilliseconds (1000)).Subscribe (l => {
+        //     fall ();
+        // }).AddTo (this);
+    }
+
+    public void fall () {
+        Vector3 pos = transform.position;
+        pos.y += -1.0f;
+        transform.position = pos;
+    }
+
+    public void MoveSide (bool right) {
+        int direction = right ? 1 : -1;
+        Vector3 pos = transform.position;
+        pos.x += 1.0f * direction;
+        transform.position = pos;
+    }
+}
---
+++ Unity_Rehabilitation/Assets/Scripts/Tetris/TetrisGameController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class TetrisGameController : MonoBehaviour {
+    [SerializeField]
+    List<GameObject> blockObjectList;
+
+    private List<GameObject> targetBlockObjectList;
+
+    private float time = 0;
+    // Start is called before the first frame update
+    void Start () {
+
+        /* 以下テストよう */
+        // ブロックを一定間隔で配置
+        targetBlockObjectList = new List<GameObject> ();
+        int blockCount = 0;
+        foreach (GameObject targetBlock in blockObjectList) {
+            float x = (blockCount / 3) * 4.0f;
+            float y = (blockCount % 3) * 4.0f;
+            GameObject createBlock = Instantiate (targetBlock, new Vector3 (x, y, 0), Quaternion.identity);
+            targetBlockObjectList.Add (createBlock);
+            blockCount++;
+        }
+
+        // 一定間隔で処理
+        Observable.Interval (TimeSpan.FromMilliseconds (5000)).Subscribe (l => {
+            // Debug.Log ("time");
+        }).AddTo (this);
+
+        // ボタン判定
+        Observable.EveryUpdate ()
+            .Where (_ => Input.GetKey (KeyCode.DownArrow))
+            .ThrottleFirst (TimeSpan.FromMilliseconds (100), Scheduler.MainThread)
+            .Subscribe (_ => {
+                foreach (GameObject block in targetBlockObjectList) {
+                    SpinObject (block);
+                }
+            });
+        Observable.EveryUpdate ()
+            .Where (_ => Input.GetKey (KeyCode.UpArrow))
+            .ThrottleFirst (TimeSpan.FromMilliseconds (100), Scheduler.MainThread)
+            .Subscribe (_ => {
+                foreach (GameObject block in targetBlockObjectList) {
+                    SpinReverseObject (block);
+                }
+            });
+        Observable.EveryUpdate ()
+            .Where (_ => Input.GetKey (KeyCode.RightArrow))
+            .ThrottleFirst (TimeSpan.FromMilliseconds (100), Scheduler.MainThread)
+            .Subscribe (_ => {
+                foreach (GameObject block in targetBlockObjectList) {
+                    Block script = block.GetComponent<Block> ();
+                    script.MoveSide (true);
+                }
+            });
+        Observable.EveryUpdate ()
+            .Where (_ => Input.GetKey (KeyCode.LeftArrow))
+            .ThrottleFirst (TimeSpan.FromMilliseconds (100), Scheduler.MainThread)
+            .Subscribe (_ => {
+                foreach (GameObject block in targetBlockObjectList) {
+                    Block script = block.GetComponent<Block> ();
+                    script.MoveSide (false);
+                }
+            });
+        Observable.EveryUpdate ()
+            .Where (_ => Input.GetKey (KeyCode.Space))
+            .ThrottleFirst (TimeSpan.FromMilliseconds (100), Scheduler.MainThread)
+            .Subscribe (_ => {
+                foreach (GameObject block in targetBlockObjectList) {
+                    Block script = block.GetComponent<Block> ();
+                    script.fall ();
+                }
+            });
+    }
+
+    // Update is called once per frame
+    void Update () { }
+
+    private void SpinObject (GameObject blockObj) {
+        blockObj.transform.Rotate (new Vector3 (0, 0, 90));
+    }
+    private void SpinReverseObject (GameObject blockObj) {
+        blockObj.transform.Rotate (new Vector3 (0, 0, -90));
+    }
+}
